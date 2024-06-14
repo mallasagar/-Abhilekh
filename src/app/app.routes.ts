eea3264740592203353543edd797e54c762a4000
malla sagar <mallasagar0101@gmail.com>
2024-06-15 00:29:44 +0545

    created register and login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,16 @@
 import { Routes } from '@angular/router';
 import { ErrorComponent } from './shared/Components/error/error.component';
 import { LandingpageComponent } from './shared/Components/landingpage/landingpage.component';
+import { LoginComponent } from './shared/Components/login/login.component';
+import { RegisterComponent } from './shared/Components/register/register.component';
 
 
 export const routes: Routes = [
         {path:'',component:LandingpageComponent},
         {path: 'admin',loadChildren: () => import('./modules/admin.Modules/admin.module').then(m => m.AdminModule) },
         {path: 'users', loadChildren: () => import('./modules/user.Modules/user.module').then(m => m.UserModule ) },
-        // {path:'login',component:AuthComponent},
-        // {path:'register',component:RegisterComponent},
+        {path:'login',component:LoginComponent},
+        {path:'register',component:RegisterComponent},
         {path:'home', pathMatch:'full', redirectTo: '/'},
         {path:'**', component:ErrorComponent}
 ];
