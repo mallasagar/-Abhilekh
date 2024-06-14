d09b50194614f22e72316d92db7e264eba5e3835
malla sagar <mallasagar0101@gmail.com>
2024-06-14 23:41:40 +0545

    added landing page animation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
+<<<<<<< Updated upstream
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [];
+=======
+// import { Routes } from '@angular/router';
+// import { AppComponent } from './app.component';
+// import { ErrorComponent } from './shared/Components/error/error.component';
+// import { LandingpageComponent } from './shared/Components/landingpage/landingpage.component';
+// import { AuthComponent } from './shared/Components/auth/auth.component';
+// import { RegisterComponent } from './shared/Components/register/register.component';
+
+
+// export const routes: Routes = [
+//         {path:'',component:LandingpageComponent},
+//         {path: 'admin',loadChildren: () => import('./modules/admin.Modules/admin.module').then(m => m.AdminModule) },
+//         {path: 'users', loadChildren: () => import('./modules/user.Modules/user.module').then(m => m.UserModule ) },
+//         {path:'login',component:AuthComponent},
+//         {path:'register',component:RegisterComponent},
+//         {path:'home', pathMatch:'full', redirectTo: '/'},
+//          {path:'**', component:ErrorComponent}
+//  ];
+>>>>>>> Stashed changes
---
+++ src/app/shared/Components/landingpage/landingpage.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-landingpage',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './landingpage.component.html',
+  styleUrl: './landingpage.component.css'
+})
+export class LandingpageComponent {
+
+}
